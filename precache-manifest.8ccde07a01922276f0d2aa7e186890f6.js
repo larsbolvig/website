b4d19f0473ddbd32360bc665ad83ab8e77c2bc35
Lars Bolvig <larsbolvig@gmail.com>
2020-04-22 14:17:30 +0200

    Updates

---
+++ precache-manifest.8ccde07a01922276f0d2aa7e186890f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374f8325b7113414998d4d53319cff7e",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "eed5f6cd568deadafcf4",
+    "url": "/website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "79db7d4981814e0cb005",
+    "url": "/website/static/js/2.455f1b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.455f1b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed5f6cd568deadafcf4",
+    "url": "/website/static/js/main.8fa138fc.chunk.js"
+  },
+  {
+    "revision": "a50d945060eddc6481b5",
+    "url": "/website/static/js/runtime-main.e471a7fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
